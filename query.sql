e4abf7303fae377d4ab9b14837927114ddb2f0fd
Kelly Adams <81118926+kellyjadams@users.noreply.github.com>
2023-02-08 15:39:43 -0800

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,2 +1,15 @@
+-- Explore the Data
+
 SELECT *
 FROM sales
+
+-- View the different platforms
+SELECT DISTINCT platform
+FROM sales
+
+-- View the years of the video games
+SELECT DISTINCT year
+FROM sales
+ORDER BY year asc
+
+
